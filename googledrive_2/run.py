8a0149d7d7c012befea8adf3a55e28b2379b1f72
subhadip1203 <subhadip1203@gmail.com>
2021-10-22 23:36:26 +0530

    pgsql-pgadmin4 docker compose

--- googledrive_2/run.py
+++ googledrive_2/run.py
@@ -1,19 +1,68 @@
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
+# import google.oauth2.credentials
+# import google_auth_oauthlib.flow
 
-flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-  'client_secret.json',
-  scopes=['https://www.googleapis.com/auth/drive.metadata.readonly']
-)
-
-
-flow.redirect_uri = 'https://www.example.com/oauth2callback'
+# flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+#   'client_secret.json',
+#   scopes=['https://www.googleapis.com/auth/drive.metadata.readonly']
+# )
 
 
-authorization_url, state = flow.authorization_url(
-  access_type='offline',
-  include_granted_scopes='true'
-)
+# flow.redirect_uri = 'https://www.example.com/oauth2callback'
 
-print(authorization_url)
-print(state)
+
+# authorization_url, state = flow.authorization_url(
+#   access_type='offline',
+#   include_granted_scopes='true'
+# )
+
+# print(authorization_url)
+# print(state)
+
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.appdata']
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+if __name__ == '__main__':
+    main()
